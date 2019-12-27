1e6cb310101b77bbe3f0e0ebc1dabfa14eb9228e
richardfan1126 <richardfan1126@gmail.com>
2019-12-27 12:24:22 +0800

    Changed method of getting local IP address
    - Changed method of getting local IP address (suggested by RayG)

--- rl_coach/env.sh
+++ rl_coach/env.sh
@@ -2,5 +2,5 @@ export AWS_ACCESS_KEY_ID=minio
 export AWS_SECRET_ACCESS_KEY=miniokey
 
 export AWS_REGION=us-east-1
-export S3_ENDPOINT_URL=http://$(hostname -i):9000
+export S3_ENDPOINT_URL=http://$(hostname -I | cut -d' ' -f1):9000
 export MODEL_S3_BUCKET=bucket
